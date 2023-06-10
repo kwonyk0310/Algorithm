7b8ad8494dc3b9495ae8bd1fdf9382b4e18abc8a
YK <ykkwon@ezens.net>
2023-06-10 15:53:35 +0900

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+
+  public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+    String str = sc.next();
+
+    for (int i = 0; i < str.length(); i++) {
+      char ch = str.charAt(i);
+      if ('A' <= ch && 'Z' >= ch)
+        System.out.print((char) ('a' + ch - 'A'));
+      else System.out.print((char) ('A' + ch - 'a'));
+    }
+  }
+
+}
